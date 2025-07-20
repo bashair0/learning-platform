afab483bb82c6f78648eec4888c6425c56acb2eb
bashair0 <bisho5.ba@gmail.com>
2025-07-20 17:48:12 +0800

    Set up drizzle

---
+++ src/drizzle/schema/course.ts
@@ -0,0 +1,18 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { relations } from "drizzle-orm";
+import { CourseProductTable } from "./courseProduct";
+import { UserCourseAccessTable } from "./userCourseAccess";
+
+export const CourseTable = pgTable("courses", {
+  id,
+  name: text().notNull(),
+  description: text().notNull(),
+  createdAt,
+  updatedAt,
+});
+
+export const CourseRelationships = relations(CourseTable, ({ many }) => ({
+  courseProducts: many(CourseProductTable),
+  userCourseAccess: many(UserCourseAccessTable),
+}));
---
+++ src/drizzle/schema/courseSection.ts
@@ -0,0 +1,35 @@
+import { pgTable, text, integer, pgEnum, uuid } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { relations } from "drizzle-orm";
+import { CourseTable } from "./course";
+import { LessonTable } from "./lesson";
+
+export const courseSectionStatuses = ["public", "private"] as const;
+export type CourseSectionStatus = (typeof courseSectionStatuses)[number];
+export const courseSectionStatusEnum = pgEnum(
+  "course_section_status",
+  courseSectionStatuses
+);
+
+export const CourseSectionTable = pgTable("course_sections", {
+  id,
+  name: text().notNull(),
+  status: courseSectionStatusEnum().notNull().default("private"),
+  order: integer().notNull(),
+  courseId: uuid()
+    .notNull()
+    .references(() => CourseTable.id, { onDelete: "cascade" }),
+  createdAt,
+  updatedAt,
+});
+
+export const CourseSectionRelationships = relations(
+  CourseSectionTable,
+  ({ many, one }) => ({
+    course: one(CourseTable, {
+      fields: [CourseSectionTable.courseId],
+      references: [CourseTable.id],
+    }),
+    lessons: many(LessonTable),
+  })
+);
