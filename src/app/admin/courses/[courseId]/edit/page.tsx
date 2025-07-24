6845529ed322473b536ca3cd488c4d89ae1d96da
bashair0 <bisho5.ba@gmail.com>
2025-07-24 14:15:43 +0800

    Finish course edit page

--- src/app/admin/courses/[courseId]/edit/page.tsx
+++ src/app/admin/courses/[courseId]/edit/page.tsx
@@ -12,6 +12,8 @@ import { SectionFormDialog } from "@/features/courseSections/components/SectionF
 import { SortableSectionList } from "@/features/courseSections/components/SortableSectionList";
 import { getCourseSectionCourseTag } from "@/features/courseSections/db/cache";
 import { deleteSection } from "@/features/courseSections/db/sections";
+import { LessonFormDialog } from "@/features/lessons/components/LessonFormDialog";
+import { SortableLessonList } from "@/features/lessons/components/SortableLessonList";
 import { getLessonCourseTag } from "@/features/lessons/db/cache/lessons";
 import { cn } from "@/lib/utils";
 import { asc, eq } from "drizzle-orm";
@@ -37,7 +39,7 @@ export default async function EditCoursePage({
           <TabsTrigger value="lessons">Lessons</TabsTrigger>
           <TabsTrigger value="details">Details</TabsTrigger>
         </TabsList>
-        <TabsContent value="lessons">
+        <TabsContent value="lessons" className="flex flex-col gap-2">
           <Card>
             <CardHeader className="flex items-center flex-row justify-between">
               <CardTitle>Sections</CardTitle>
@@ -57,6 +59,35 @@ export default async function EditCoursePage({
               />
             </CardContent>
           </Card>
+          <hr className="my-2" />
+          {course.courseSections.map((section) => (
+            <Card key={section.id}>
+              <CardHeader className="flex items-center flex-row justify-between gap-4">
+                <CardTitle
+                  className={cn(
+                    "flex items-center gap-2",
+                    section.status === "private" && "text-muted-foreground"
+                  )}>
+                  {section.status === "private" && <EyeClosed />} {section.name}
+                </CardTitle>
+                <LessonFormDialog
+                  defaultSectionId={section.id}
+                  sections={course.courseSections}>
+                  <DialogTrigger asChild>
+                    <Button variant="outline">
+                      <PlusIcon /> New Lesson
+                    </Button>
+                  </DialogTrigger>
+                </LessonFormDialog>
+              </CardHeader>
+              <CardContent>
+                <SortableLessonList
+                  sections={course.courseSections}
+                  lessons={section.lessons}
+                />
+              </CardContent>
+            </Card>
+          ))}
         </TabsContent>
         <TabsContent value="details">
           <Card>
--- src/lib/dataCache.ts
+++ src/lib/dataCache.ts
@@ -4,7 +4,8 @@ type CACHE_TAG =
   | "courses"
   | "userCourseAccess"
   | "courseSections"
-  | "lessons";
+  | "lessons"
+  | "userLessonComplete";
 
 export function getGlobalTag(tag: CACHE_TAG) {
   return `global:${tag}` as const;
